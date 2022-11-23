195bed395701c5bd96d76d4a255e73b7e7497c68
duongham2511 <duongham2511@gmail.com>
2022-11-23 19:59:57 +0700

    dummy minimax implementation

--- Assets/Scripts/AI/CheckersSimulation.cs
+++ Assets/Scripts/AI/CheckersSimulation.cs
@@ -10,7 +10,7 @@ public class CheckersSimulation : MonoBehaviour
 
     public (Vector2Int, Vector2Int) Minimax(Transform[,] board, int depth, int turn, int red_best, int black_best)
     {
-        
+        return (Vector2Int.zero, Vector2Int.zero);
     }
 
     private void OnDrawGizmos()
